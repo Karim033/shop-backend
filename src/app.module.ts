11018a250ac9971efca273e67c69d94145dec283
Karim033 <gaberk033@gmail.com>
2024-09-02 14:41:15 +0300

    Creating payment entity and handling one to one relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
 import { EnvModule } from './env/env.module';
 import { OrderModule } from './domain/order/order.module';
+import { PaymentsModule } from './domain/payments/payments.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, DatabaseModule, EnvModule, OrderModule],
+  imports: [UsersModule, CommonModule, DatabaseModule, EnvModule, OrderModule, PaymentsModule],
 })
 export class AppModule {}
