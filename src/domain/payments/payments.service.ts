f2c5a6fcd8795432fa830534fd6773104ceb1f7b
Karim033 <gaberk033@gmail.com>
2024-09-03 20:35:26 +0300

    Impleminting payment login

--- src/domain/payments/payments.service.ts
+++ src/domain/payments/payments.service.ts
@@ -1,26 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from 'order/entities/order.entity';
+import { Repository } from 'typeorm';
+import { Payment } from './entities/payment.entity';
+import { OrderStatus } from 'order/enums/order-status.enum';
 
 @Injectable()
 export class PaymentsService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
-  }
-
-  findAll() {
-    return `This action returns all payments`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} payment`;
-  }
-
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} payment`;
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
+  ) {}
+  async payOrder(id: number) {
+    const order = await this.orderRepository.findOne({
+      where: { id },
+      relations: {
+        payment: true,
+      },
+    });
+    if (!order) {
+      throw new NotFoundException(`order not found with id: ${id}`);
+    }
+    if (order.payment) {
+      throw new NotFoundException(`order already paid with id: ${id}`);
+    }
+    const payment = this.paymentRepository.create();
+    order.payment = payment;
+    order.status = OrderStatus.AWAITING_SHIPMENT;
+    return this.orderRepository.save(order);
   }
 }
