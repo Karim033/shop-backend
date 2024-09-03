f72a4adb11643d0b8bf744b4f42b669e08d3f370
Karim033 <gaberk033@gmail.com>
2024-09-03 20:21:06 +0300

    Using serialization to return get fileds

--- src/domain/order/entities/order-item.entity.ts
+++ src/domain/order/entities/order-item.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 import { Order } from './order.entity';
 import { Product } from 'products/entities/product.entity';
+import { Expose } from 'class-transformer';
 
 @Entity()
 export class OrderItem {
@@ -17,4 +18,9 @@ export class OrderItem {
   quantity: number;
   @Column({ type: 'decimal', precision: 6, scale: 2 })
   price: number;
+
+  @Expose()
+  get subTotal() {
+    return this.quantity * this.price;
+  }
 }
