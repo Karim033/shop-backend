6c97eb5d6ccb87e18b55e8581ff64bc3c1076157
Karim033 <gaberk033@gmail.com>
2024-09-08 16:59:17 +0300

    Creating a swagger base document

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('The Shop API')
+    .setDescription('Documentation for the Shop API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
