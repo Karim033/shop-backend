a5e1adf8d7aa1fdd5e5226d33e443f84a3580f46
Karim033 <gaberk033@gmail.com>
2024-09-18 18:15:19 +0300

    sENDING PAGE DIECTLY AND MAX VALUE IN PAGINATION DTO

--- src/quering/dto/pagination.dto.ts
+++ src/quering/dto/pagination.dto.ts
@@ -1,12 +1,15 @@
-import { IsOptional } from 'class-validator';
+import { IsOptional, Max } from 'class-validator';
 import { IsCadinal } from 'common/decorators/validators/is-cardinal.decorator';
+import { MAX_PAGE_NUMBER, MAX_PAGE_SIZE } from 'quering/util/quering.constant';
 
 export class PaginationDto {
   @IsOptional()
-  @IsCadinal()
-  readonly offset?: number;
-
-  @IsOptional()
+  @Max(MAX_PAGE_SIZE)
   @IsCadinal()
   readonly limit?: number;
+
+  @IsOptional()
+  @Max(MAX_PAGE_NUMBER)
+  @IsCadinal()
+  readonly page?: number = 1;
 }
